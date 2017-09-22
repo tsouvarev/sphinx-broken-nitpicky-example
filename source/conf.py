bbd670755a06ad9a6bdbffd350e950bfde1807fd
Ivan Tsouvarev <tsouvarev@gmail.com>
2017-09-22 17:40:15 +0300

    Setup django in conf.py
    Needed to be able to reference models and django-specific stuff in docs

--- source/conf.py
+++ source/conf.py
@@ -16,10 +16,15 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nitpicky.settings")
+
+import django
+django.setup()
 
 
 # -- General configuration ------------------------------------------------
